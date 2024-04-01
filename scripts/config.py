8cb094186162e675791b6a8c671a5c21cdffd1a9
vikramsingh <singh.vikram.1782000@gmail.com>
2024-04-01 09:22:44 +0000

    commit

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,15 @@
 LOGS_DIR = '../logs'
-Dataset_Name = "AlekseyKorshuk/product-photography-all"
 DATA_DIR = '../data'
 Project_Name = 'product_placement_api'
 entity = 'vikramxd'
 image_dir = '../sample_data'
 mask_dir = '../masks'
+segmentation_model = 'facebook/sam-vit-large'
+detection_model = 'yolov8l'
 controlnet_adapter_model_name= 'lllyasviel/control_v11p_sd15_inpaint'
 controlnet_base_model_name = "runwayml/stable-diffusion-inpainting"
 kandinsky_model_name = 'kandinsky-community/kandinsky-2-2-decoder-inpaint'
 video_model_name = 'stabilityai/stable-video-diffusion-img2vid-xt'
+target_width = 2560
+target_height = 1440
+roi_scale = 0.6
--- scripts/run.py
+++ scripts/run.py
@@ -1,55 +1,39 @@
 import argparse
 import os
-from mask_generator import  invert_mask
-from segment_everything import generate_mask_from_bbox
+from segment_everything import generate_mask_from_bbox, extend_image, invert_mask
 from models import kandinsky_inpainting_inference, load_image
 from PIL import Image
-from mask_generator import extend_image
-
+from config import segmentation_model, detection_model,target_height, target_width, roi_scale
 
 def main(args):
-    # Create directories if they don't exist
+    """
+    Main function that performs the product diffusion process.
+
+    Args:
+        args (Namespace): Command-line arguments.
+
+    Returns:
+        None
+    """
     os.makedirs(args.output_dir, exist_ok=True)
     os.makedirs(args.mask_dir, exist_ok=True)
-
-    # Extend image
-    extended_image = extend_image(image_path=args.image_path, target_width=2560, target_height=1440, roi_scale=0.7)
-    extended_image_path = os.path.join(args.output_dir, f'{args.uid}_extended.jpg')
-    extended_image.save(extended_image_path)
-
-    # Generate mask from extended image
-    mask = generate_mask_from_bbox(extended_image_path)
-    mask_image = Image.fromarray(mask)
-
-    # Save original mask
-    original_mask_path = os.path.join(args.mask_dir, f'{args.uid}_original_mask.jpg')
-    mask_image.save(original_mask_path)
-
-    # Invert mask
-    mask_image = load_image(original_mask_path)
-    inverted_mask = invert_mask(mask_image)
-    inverted_mask_path = os.path.join(args.mask_dir, f'{args.uid}_inverted_mask.jpg')
-    inverted_mask.save(inverted_mask_path)
-
-    # Load mask and extended image
-    mask_image = load_image(inverted_mask_path)
-    extended_image = load_image(extended_image_path)
-
-    # Perform inpainting on extended image
-    output_image = kandinsky_inpainting_inference(args.prompt, args.negative_prompt, extended_image, mask_image)
-
-    # Save output image
     output_image_path = os.path.join(args.output_dir, f'{args.uid}_output.jpg')
+    image = load_image(args.image_path)
+    extended_image = extend_image(image, target_width=target_width, target_height=target_height, roi_scale=roi_scale)
+    mask = generate_mask_from_bbox(extended_image, segmentation_model, detection_model)
+    mask_image = Image.fromarray(mask)
+    inverted_mask = invert_mask(mask_image)
+    #inverted_mask = Image.fromarray(inverted_mask)
+    output_image = kandinsky_inpainting_inference(args.prompt, args.negative_prompt, extended_image, inverted_mask)
     output_image.save(output_image_path)
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Perform Kandinsky inpainting on an image.')
+    parser = argparse.ArgumentParser(description='Perform Outpainting on an image.')
     parser.add_argument('--image_path', type=str, required=True, help='Path to the input image.')
     parser.add_argument('--prompt', type=str, required=True, help='Prompt for the Kandinsky inpainting.')
     parser.add_argument('--negative_prompt', type=str, required=True, help='Negative prompt for the Kandinsky inpainting.')
     parser.add_argument('--output_dir', type=str, required=True, help='Directory to save the output image.')
     parser.add_argument('--mask_dir', type=str, required=True, help='Directory to save the mask image.')
     parser.add_argument('--uid', type=str, required=True, help='Unique identifier for the image and mask.')
-
     args = parser.parse_args()
     main(args)
