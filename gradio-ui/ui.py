19723694423fd1c375b6063244b470f5e4b76b40
Vikramjeet <singh.vikram.1782000@gmail.com>
2024-06-10 20:24:55 +0000

    chore: Add generate button to gradio UI for SDXL LORA Inpainting
    Former-commit-id: afc058d6c6c4baf3747e59e20deadd8ba163d2a9

--- gradio-ui/ui.py
+++ gradio-ui/ui.py
@@ -3,7 +3,7 @@ import requests
 from pydantic import BaseModel
 from diffusers.utils import load_image
 
-# Define your API endpoint
+
 SDXL_LORA_API_URL = 'http://127.0.0.1:8000/api/v1/product-diffusion/sdxl_v0_lora_inference'
 
 # Define the InpaintingRequest model
@@ -39,7 +39,7 @@ with gr.Blocks(theme='WeixuanYuan/Soft_dark') as demo:
               
                     prompt = gr.Textbox(label="Prompt", placeholder="Enter your prompt here")
                     negative_prompt = gr.Textbox(label="Negative Prompt", placeholder="Enter negative prompt here")
-                    generate_button = gr.Button("Generate Image")
+                   
 
                
                     with gr.Column(scale=1):
@@ -47,10 +47,11 @@ with gr.Blocks(theme='WeixuanYuan/Soft_dark') as demo:
                         guidance_scale = gr.Slider(minimum=1.0, maximum=10.0, step=0.1, value=7.5, label="Guidance Scale")
                         num_images = gr.Slider(minimum=1, maximum=10, step=1, value=1, label="Number of Images")
                         mode = gr.Dropdown(choices=["s3_json", "b64_json"], value="s3_json", label="Mode")
+                        generate_button = gr.Button("Generate Image")
             
             
             image_preview = gr.Image(label="Generated Image", height=512, width=512,scale=1,show_download_button=True,show_share_button=True,container=False)
-
+        
         generate_button.click(generate_sdxl_lora_image, inputs=[prompt, negative_prompt, num_inference_steps, guidance_scale, num_images, mode], outputs=[image_preview])
 
 demo.launch()
