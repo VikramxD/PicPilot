09ce98e59e67c025c88353bb651fbf7b284c6200
Vikram <singh.vikram.1782000@gmail.com>
2024-06-23 13:38:29 +0530

    chore: Add LOGFIRE_TOKEN to config_settings.py

--- config_settings.py
+++ config_settings.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    LOGFIRE_TOKEN:str = ''
     AWS_ACCESS_KEY_ID: str = ''
     AWS_SECRET_ACCESS_KEY: str = ''
     AWS_REGION: str = "ap-south-1"
