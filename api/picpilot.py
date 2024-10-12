516ef9269bc4359323174d0cf7f441339510edc0
Vikramjeet <singh.vikram.1782000@gmail.com>
2024-10-12 20:55:13 +0000

    chore: Update port number for servers in picpilot.py

--- api/picpilot.py
+++ api/picpilot.py
@@ -11,6 +11,6 @@ if __name__=="__main__":
     sdxl_server = LitServer(SDXLLoraAPI(),api_path='/api/v2/generate/sdxl',accelerator="auto",max_batch_size=tti_settings.MAX_BATCH_SIZE,batch_timeout=tti_settings.MAX_QUEUE_DELAY_MICROSECONDS / 1e6)
     outpainting_server = LitServer(OutpaintingAPI(),api_path='/api/v2/painting/sdxl_outpainting',accelerator='auto',max_batch_size=4,batch_timeout=0.1)
     image2video_server = LitServer(Image2VideoAPI(),api='/api/v2/image2video/cogvideox',accelerator='auto',max_batch_size=1,batch_timeout=0.1)
-    run_all([flux_server,sdxl_server,outpainting_server,image2video_server], port=8000)
+    run_all([flux_server,sdxl_server,outpainting_server,image2video_server], port=7860)
     
    
