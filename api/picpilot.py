0e55855aee0338184422e1ae41fb717b926630d3
Vikramjeet <singh.vikram.1782000@gmail.com>
2024-10-14 16:20:43 +0530

    Update picpilot.py and requirements.txt for runpod integration

--- api/picpilot.py
+++ api/picpilot.py
@@ -4,6 +4,7 @@ from sdxl_serve import SDXLLoraAPI
 from configs.tti_settings import tti_settings
 from outpainting_serve import OutpaintingAPI
 from image2video_serve import ImageToVideoAPI
+import runpod
 
 
 if __name__=="__main__":
@@ -11,6 +12,6 @@ if __name__=="__main__":
     sdxl_server = LitServer(SDXLLoraAPI(),api_path='/api/v2/generate/sdxl',accelerator="auto",max_batch_size=tti_settings.MAX_BATCH_SIZE,batch_timeout=tti_settings.MAX_QUEUE_DELAY_MICROSECONDS / 1e6)
     outpainting_server = LitServer(OutpaintingAPI(),api_path='/api/v2/painting/sdxl_outpainting',accelerator='auto',max_batch_size=4,batch_timeout=0.1)
     image2video_server = LitServer(ImageToVideoAPI(),api_path='/api/v2/image2video/cogvideox',accelerator='auto',max_batch_size=1,batch_timeout=0.1)
-    run_all([flux_server,sdxl_server,outpainting_server,image2video_server], port=7860)
+    runpod(run_all([flux_server,sdxl_server,outpainting_server,image2video_server], port=7860))
     
    
